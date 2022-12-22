9fd66b90843f71e9a4af9ca3ae52aff331c69604
bhavikm02 <121260390+bhavikm02@users.noreply.github.com>
2022-12-22 15:16:39 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -2,7 +2,8 @@
 namespace HelloWorld
 {
     //Class comments added
-    class Hello {         
+    class Hello { 
+        //Added Method level comments
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
